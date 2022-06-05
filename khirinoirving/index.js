865164b802e19b8f3f6cec05627a9bcea33965af
Khiri noirving :) <55616002+supimkhi@users.noreply.github.com>
2022-06-05 12:48:26 -0400

    Add files via upload

---
+++ khirinoirving/index.js
@@ -0,0 +1,10 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+window.onload = function(){
+  document.getElementById("").play();
+}
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
